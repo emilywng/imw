f11c89c9b883617869aaa693da1e09316614dd29
emilywng <emilyy-wangg@hotmail.com>
2024-04-10 00:30:18 -0400

    final indexx

---
+++ p2.js
@@ -0,0 +1,52 @@
+// click function to change image
+
+$(document).ready(function(){
+    $("#cat9").click(function(){
+        // Change src attribute of image
+        $(cat9).attr("src", "images/portrait9-1.png");
+    });    
+});
+
+$(document).ready(function(){
+    $("#cat10").click(function(){
+        // Change src attribute of image
+        $(cat10).attr("src", "images/portrait10-1.png");
+    });    
+});
+
+$(document).ready(function(){
+    $("#cat11").click(function(){
+        // Change src attribute of image
+        $(cat11).attr("src", "images/portrait11-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat12").click(function(){
+        // Change src attribute of image
+        $(cat12).attr("src", "images/portrait12-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat13").click(function(){
+        // Change src attribute of image
+        $(cat13).attr("src", "images/portrait13-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat14").click(function(){
+        // Change src attribute of image
+        $(cat14).attr("src", "images/portrait14-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat15").click(function(){
+        // Change src attribute of image
+        $(cat15).attr("src", "images/portrait15-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat16").click(function(){
+        // Change src attribute of image
+        $(cat16).attr("src", "images/portrait16-1.png");
+    });    
+});
---
+++ script.js
@@ -0,0 +1,52 @@
+// click function to change image
+
+$(document).ready(function(){
+    $("#cat1").click(function(){
+        // Change src attribute of image
+        $(cat1).attr("src", "images/portrait1-1.png");
+    });    
+});
+
+$(document).ready(function(){
+    $("#cat2").click(function(){
+        // Change src attribute of image
+        $(cat2).attr("src", "images/portrait2-1.png");
+    });    
+});
+
+$(document).ready(function(){
+    $("#cat3").click(function(){
+        // Change src attribute of image
+        $(cat3).attr("src", "images/portrait3-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat4").click(function(){
+        // Change src attribute of image
+        $(cat4).attr("src", "images/portrait4-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat5").click(function(){
+        // Change src attribute of image
+        $(cat5).attr("src", "images/portrait5-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat6").click(function(){
+        // Change src attribute of image
+        $(cat6).attr("src", "images/portrait6-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat7").click(function(){
+        // Change src attribute of image
+        $(cat7).attr("src", "images/portrait7-1.png");
+    });    
+});
+$(document).ready(function(){
+    $("#cat8").click(function(){
+        // Change src attribute of image
+        $(cat8).attr("src", "images/portrait8-1.png");
+    });    
+});
